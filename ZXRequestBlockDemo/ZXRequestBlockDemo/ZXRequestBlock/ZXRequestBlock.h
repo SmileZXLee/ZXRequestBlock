0902cfa97bda580abf04ea915f93d03ca525a704
李兆祥 <393727164@qq.com>
2019-08-24 22:30:20 +0800

    Update ZXRequestBlock.h

--- ZXRequestBlockDemo/ZXRequestBlockDemo/ZXRequestBlock/ZXRequestBlock.h
+++ ZXRequestBlockDemo/ZXRequestBlockDemo/ZXRequestBlock/ZXRequestBlock.h
@@ -10,7 +10,23 @@
 typedef NSURLRequest *(^requestBlock) (NSURLRequest *request);
 @interface ZXRequestBlock : NSObject
 @property (nonatomic, copy) NSURLRequest *(^requestBlock)(NSURLRequest *request);
+
+/**
+ 拦截全局请求
+
+ @param block 请求回调，block返回修改后的请求
+ */
 +(void)handleRequest:(requestBlock)block;
+
+/**
+ 启用HttpDns
+ */
 +(void)enableHttpDns;
+
+/**
+ 禁止网络代理（开启后使用代理方式抓包的程序无法抓到此App中的请求）
+
+ @return 若不为nin，则代表检测到了网络代理，可进行额外操作
+ */
 +(id)disableHttpProxy;
 @end
